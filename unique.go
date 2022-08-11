0f9e77405b9021020d8b599452fe153ee2d68883
Leon Klingele <leon.klingele@helsinki-systems.de>
2022-08-11 14:03:12 +0200

    Move unique memory storage to its own file

--- unique.go
+++ unique.go
@@ -1,10 +1,5 @@
 package submissionlimit
 
-import (
-	"fmt"
-	"sync"
-)
-
 type UniqueConfig struct {
 	Storage uniqueStorage
 }
@@ -38,37 +33,5 @@ func WithUnique(uc UniqueConfig) option {
 
 	return func(l *Limiter) {
 		l.uniqueLimiter = ul
-	}
-}
-
-type UniqueMemoryStorage struct {
-	store   map[string][]string
-	storeMu sync.Mutex
-}
-
-var _ uniqueStorage = (*UniqueMemoryStorage)(nil)
-
-func (ums *UniqueMemoryStorage) Store(k, v string) error {
-	ums.storeMu.Lock()
-	defer ums.storeMu.Unlock()
-
-	if svs, ok := ums.store[k]; !ok {
-		ums.store[k] = []string{v}
-	} else {
-		for _, sv := range svs {
-			if sv == v {
-				return fmt.Errorf("key %q not unique", k)
-			}
-		}
-
-		ums.store[k] = append(ums.store[k], v)
-	}
-
-	return nil
-}
-
-func NewUniqueMemoryStorage() *UniqueMemoryStorage {
-	return &UniqueMemoryStorage{
-		store: make(map[string][]string),
 	}
 }
